dc8f03abb52e3ff81bbdbdc0b390934de8e70f28
tcorrell02 <55494824+tcorrell02@users.noreply.github.com>
2019-09-18 09:07:32 -0400

    Create startOfGame.java

---
+++ startOfGame.java
@@ -0,0 +1,14 @@
+public class startOfGame{
+    String name;
+    String characterClass;
+    public nullBackground(){
+        name = “”;
+        characterClass = “”;
+    }
+    public void gameBackground(){
+        System.out.println("Welcome to the world of Javah, your typical fantasy game.");
+      
+        System.out.print("In this world you must battle monsters and gain strength in order to defeat the five titans.");
+        System.out.print("What is your name, brave adventurer?");       
+    }
+}
