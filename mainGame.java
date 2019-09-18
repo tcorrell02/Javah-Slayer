0c466c7f8a7ae497b5f88bc91e20b70b4a8d3874
tcorrell02 <55494824+tcorrell02@users.noreply.github.com>
2019-09-18 09:06:44 -0400

    Create mainGame.java

---
+++ mainGame.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class javahSlayer{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        startOfGame background = new nullBackground();
+        background.gameBackground();
+    
+                
+        
+        
+    
+    }
+    
+}
